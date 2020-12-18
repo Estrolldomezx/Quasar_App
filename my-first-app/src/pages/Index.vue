62bcc052c60c43b6a11672a1ab9b1f661ec3e97b
Wanish Sengkhim <wanish45037.2@gmail.com>
2020-12-19 01:14:43 +0700

    first commit

---
+++ my-first-app/src/pages/Index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="q-pa-md">
+    <q-option-group
+      v-model="controlType"
+      :options="controlTypeOptions"
+      color="purple"
+      inline
+      class="q-mb-md"
+    />
+    <q-carousel
+      v-model="slide"
+      swipeable
+      animated
+      :control-type="controlType"
+      control-color="purple"
+      navigation
+      padding
+      arrows
+      height="80vh"
+      class="text-purple rounded-borders"
+    >
+      <q-carousel-slide :name="1" img-src="https://cdn.quasar.dev/img/mountains.jpg">
+      </q-carousel-slide>
+      <q-carousel-slide :name="2" img-src="https://cdn.quasar.dev/img/parallax1.jpg">
+      </q-carousel-slide>
+      <q-carousel-slide :name="3" img-src="https://cdn.quasar.dev/img/parallax2.jpg">
+      </q-carousel-slide>
+      <q-carousel-slide :name="4" img-src="https://cdn.quasar.dev/img/quasar.jpg" >
+      </q-carousel-slide>
+    </q-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      slide: 1
+    }
+  }
+}
+
+</script>
